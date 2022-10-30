fb102f8416d7183111ec144b4a839c69ab29c0fe
Mohammad Ali <swe.mirza.ali@gmail.com>
2022-10-30 18:37:42 +0000

    feat: new changes 31/10/2022

---
+++ mdpl_customization/mdpl_customization/report/sales_invoice_payment_entry_custom_report/sales_invoice_payment_entry_custom_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, vansh and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Sales Invoice Payment Entry Custom Report"] = {
+	"filters": [
+
+	]
+};
