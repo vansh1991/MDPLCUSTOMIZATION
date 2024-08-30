81699ad631c80ce5fe732d6273994e8e1bb234e0
bizmapNikhil <nikhil.tulankar@bizmap.in>
2024-08-30 04:49:10 +0200

    added new filter for apple id and zero row values

--- mdpl_customization/mdpl_customization/report/sales_invoice_script_report/sales_invoice_script_report.js
+++ mdpl_customization/mdpl_customization/report/sales_invoice_script_report/sales_invoice_script_report.js
@@ -1,3 +1,4 @@
+
 frappe.query_reports["sales invoice script report"] = {
     "filters": [
         {
@@ -14,17 +15,36 @@ frappe.query_reports["sales invoice script report"] = {
             "default": frappe.datetime.now_date(),
             "reqd": 1
         },
-        {   
+        {
             "fieldname": "itm_group",
             "label": "Item Group",
             "fieldtype": "MultiSelectList",
             "options": "Item Group",
             "get_data": function(txt) {
-                return frappe.db.get_link_options("Item Group", txt);
+            // If there is search text, use frappe.db.get_link_options to get the results
+            if (txt) {
+               return frappe.db.get_link_options("Item Group", txt);
             }
-        },
-        
-        {
+
+            // Otherwise, return the static filtered data based on allowed parent item groups
+            const allowed_parent_groups = [
+               "Demo", "Accessories", "AirPods", "Apple Watch", "iPad", "iPhone", "Macbook"
+             ];
+
+           // Fetch child item groups based on the allowed parent groups
+           return frappe.db.get_list('Item Group', {
+              fields: ["name"],
+              filters: {
+                parent_item_group: ["in", allowed_parent_groups]
+               }
+             }).then(data => {
+            // Return all the filtered item groups when no search text is provided
+            return data.map(d => ({ value: d.name, label: d.name }));
+           });
+         }
+       },
+           
+       {
             "fieldname": "customer",
             "label": "Customer",
             "fieldtype": "MultiSelectList",
@@ -33,22 +53,37 @@ frappe.query_reports["sales invoice script report"] = {
                 return frappe.db.get_link_options("Customer", txt);
             }
         },
-         {
-    "fieldname": "parent_item_group",
-    "label": "Parent Item Group",
-    "fieldtype": "MultiSelectList",
-    "options": "Item Group",
-    "get_data": function(txt) {
-        return frappe.db.get_list('Item Group', {
-            fields: ["name"],
-            filters: {
-                is_group: 1
-            }
-        }).then(data => {
-            return data.map(d => ({ value: d.name, label: d.name }));
-        });
-    }
-}
+        {
+        "fieldname": "parent_item_group",
+        "label": "Parent Item Group",
+        "fieldtype": "MultiSelectList",
+        "options": "Item Group",
+        "get_data": function(txt) {
+
+            
+    // "Accessories", "AirPods", "Apple Watch", "iPad", "iPhone", "Macbook",
+            return frappe.db.get_list('Item Group', {
+                fields: ["name"],
+                filters: {
+                    parent_item_group: "Apple"
+                }
+            }).then(data => {
+
+                return data
+
+                    .map(d => ({ value: d.name, label: d.name }));
+            });
+        }
+    },
+
+    {
+        "fieldname": "apple_id",
+        "label": "Apple Id",
+        "fieldtype": "Check",
+        "default": 0,
+    
+    }, 
+
 
 
     ]
--- mdpl_customization/mdpl_customization/report/sales_invoice_script_report/sales_invoice_script_report.py
+++ mdpl_customization/mdpl_customization/report/sales_invoice_script_report/sales_invoice_script_report.py
@@ -14,7 +14,9 @@ def execute(filters=None):
         '11', '12', '13', '14', '14 Plus', '14 Pro', '14 Pro Max',
         '15', '15 Plus', '15 Pro', '15 Pro Max', 'iPad Air 6th gen',
         'iPad Pro 5th gen', 'Ipad 9th gen', 'iPad 10th gen',
-        'Ipad Air 5th gen', 'Ipad pro 4th gen', 'iPad Pro 3rd Gen'
+        'Ipad Air 5th gen', 'Ipad pro 4th gen', 'iPad Pro 3rd Gen', '(Accessories)',
+        'Watch SE 2', 'Watch 8', 'WATCH Ultra', 'Watch 9', 'WATCH Ultra 2',
+        'Mac Air M2', 'Mac Air M3'
     ]
 
     # Initialize selected_item_groups
@@ -22,13 +24,19 @@ def execute(filters=None):
 
     # Apply parent_item_group logic first
     if filters.get('parent_item_group'):
+        if 'Macbook' in filters['parent_item_group']:
+            selected_item_groups += ['Mac Air M2', 'Mac Air M3']
         if 'AirPods' in filters['parent_item_group']:
             selected_item_groups += ['Airpod 2', 'Airpod 3', 'Airpod Pro', 'Airpod Pro2']
         if 'iPhone' in filters['parent_item_group']:
             selected_item_groups += ['11', '12', '13', '14', '14 Plus', '14 Pro', '14 Pro Max', '15', '15 Plus', '15 Pro', '15 Pro Max']
         if 'iPad' in filters['parent_item_group']:
             selected_item_groups += ['iPad Air 6th gen', 'iPad Pro 5th gen', 'Ipad 9th gen', 'iPad 10th gen', 'Ipad Air 5th gen', 'Ipad pro 4th gen', 'iPad Pro 3rd Gen']
-
+        if 'Accessories' in filters['parent_item_group']:
+            selected_item_groups += ['(Accessories)']
+        if 'Apple Watch' in filters['parent_item_group']:
+            selected_item_groups += ['Watch SE 2', 'Watch 8', 'WATCH Ultra', 'Watch 9', 'WATCH Ultra 2']
+        
     # If itm_group is selected, prioritize itm_group over parent_item_group
     if filters.get('itm_group'):
         selected_item_groups = filters['itm_group']
@@ -73,6 +81,10 @@ def execute(filters=None):
         customer_placeholders = ', '.join(['%s'] * len(filters['customer']))
         where_clause += f" AND si.customer IN ({customer_placeholders})"
 
+    # Add apple_id filter, ensuring it is not empty if the filter is checked
+    if filters.get('apple_id'):
+        where_clause += " AND c.apple_id IS NOT NULL AND c.apple_id != ''"
+
     # Construct the final query
     query = f"""
     SELECT 
@@ -83,6 +95,7 @@ def execute(filters=None):
     INNER JOIN `tabSales Invoice Item` si_item ON si.name = si_item.parent
     INNER JOIN `tabItem` item ON si_item.item_code = item.name
     INNER JOIN `tabItem Group` ig ON item.item_group = ig.name
+    INNER JOIN `tabCustomer` c ON si.customer = c.name
     WHERE  
         {where_clause}
     GROUP BY 
@@ -90,7 +103,25 @@ def execute(filters=None):
     ORDER BY 
         si.customer;
     """
+    # query = f"""
+    # SELECT 
+    #     c.name AS customer,
+    #     {group_select_clause}
+    # FROM 
+    #     `tabCustomer` c
+    # LEFT JOIN `tabSales Invoice` si ON c.name = si.customer
+    # LEFT JOIN `tabSales Invoice Item` si_item ON si.name = si_item.parent
+    # LEFT JOIN `tabItem` item ON si_item.item_code = item.name
+    # LEFT JOIN `tabItem Group` ig ON item.item_group = ig.name
+    # WHERE  
+    #     {where_clause}  -- Adjust this clause to handle cases where si is NULL, if necessary
+    # GROUP BY 
+    #     c.name
+    # ORDER BY 
+    #     c.name;
+    # """
 
+    
     # Add filter parameters for the query
     params = [filters.get('from_date'), filters.get('to_date')]
 
@@ -101,17 +132,49 @@ def execute(filters=None):
         params.extend(filters['parent_item_group'])
     if filters.get('customer'):
         params.extend(filters['customer'])
+    # if filters.get('apple_id') is not None:
+    #     params.append(filters['apple_id'])
+        
 
     # Log query and parameters for debugging
     frappe.logger().info(f"Executing query: {query}")
     frappe.logger().info(f"With params: {params}")
 
+    
     try:
-        # Execute the SQL query
         data = frappe.db.sql(query, params, as_dict=True)
+        
     except Exception as e:
         frappe.logger().error(f"Error executing query: {e}")
         raise
+    
+    
+    if filters.get("itm_group"):
+        all_customer = frappe.db.sql("""SELECT name AS customer FROM `tabCustomer`""", as_dict=True)
+        
+        for customer_row in all_customer:
+            if customer_row.get("customer") not in [customer.get("customer") for customer in data]:
+                data.append(customer_row)
+
+    # If no data is returned, return a single row with 0 values
+    if not data:
+        customer_query = "SELECT name AS customer FROM `tabCustomer` WHERE disabled = 0"
+        
+        # Apply customer filter
+        if filters.get('customer'):
+            customer_query += f" AND name IN ({customer_placeholders})"
+            params = filters['customer']
+        else:
+            params = []
+
+        # Fetch all customers
+        all_customers = frappe.db.sql(customer_query, params, as_dict=True)
+      
+        # Prepare data with zero values
+        data = [{
+            "customer": customer.get('customer'),
+            **{sanitized_group: 0 for sanitized_group in sanitized_groups},
+        } for customer in all_customers]
 
     # Dynamically add columns for each selected item group
     columns.extend({
@@ -132,6 +195,6 @@ def execute(filters=None):
     # Calculate the total column for each row
     for row in data:
         row['total'] = sum(row.get(sanitized_group, 0) for sanitized_group in sanitized_groups)
-
+    
     return columns, data
 
