b83c4500e4e31ffc97c88b9795a669a381ed6c56
shobasobi90 <shobasobi90@gmail.com>
2025-11-03 15:00:44 +0530

    Update __init__.py

--- mdpl_customization/__init__.py
+++ mdpl_customization/__init__.py
@@ -1,14 +1,7 @@
 __version__ = '0.0.1'
 
-import frappe
+from erpnext.controllers import sales_and_purchase_return as sra 
+from mdpl_customization.utils import validate_return_against as cra
 
-try:
-    # Import ERPNext controller (only works if ERPNext is already installed)
-    from erpnext.controllers import sales_and_purchase_return as sra
-    from mdpl_customization.utils import validate_return_against as cra
 
-    # Override ERPNext's function
-    sra.validate_return_against = cra
-
-except ImportError:
-    frappe.logger().warning("ERPNext not found — skipping return validation override during build.")
+sra.validate_return_against=cra
