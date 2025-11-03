a850edf6d607dff7978f3f4a303d5538ff6b7243
shobasobi90 <shobasobi90@gmail.com>
2025-11-03 15:26:15 +0530

    Update __init__.py

--- mdpl_customization/__init__.py
+++ mdpl_customization/__init__.py
@@ -1,7 +1,18 @@
 __version__ = '0.0.1'
 
-from erpnext.controllers import sales_and_purchase_return as sra 
-from mdpl_customization.utils import validate_return_against as cra
+def patch_sales_and_purchase_return():
+    try:
+        from erpnext.controllers import sales_and_purchase_return as sra
+        from mdpl_customization.utils import validate_return_against as cra
+        sra.validate_return_against = cra
+    except ModuleNotFoundError:
+        # frappe/erpnext not available during build
+        pass
 
-
-sra.validate_return_against=cra
+# run patch after frappe loads
+try:
+    import frappe
+    frappe.ready += patch_sales_and_purchase_return
+except Exception:
+    # ignore build-time errors
+    pass
